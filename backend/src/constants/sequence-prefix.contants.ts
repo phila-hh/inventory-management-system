8629646a2c9ec6363073608abeb7741c24de88c2
Danielkindeya <166437383+Danielkindeya@users.noreply.github.com>
2026-01-11 16:21:41 +0300

    feat(backend): add core utilities and DTOs
    - Add sequence prefix constants
    - Add OK response DTO
    - Add ObjectId validation pipe

---
+++ backend/src/constants/sequence-prefix.contants.ts
@@ -0,0 +1,11 @@
+export enum SequencePrefix {
+  MEDICATION = 'MED',
+  PATIENT = 'PAT',
+  BATCH = 'BA',
+  PRESCRIPTION = 'PRE',
+  INVOICE = 'INV',
+  PAYMENT = 'PAY',
+  ORDER = 'ORD',
+  SUPPLIER = 'SUP',
+  CUSTOMER = 'CUS',
+}
